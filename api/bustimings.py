e5bc6e1ce51416f1a2b34772d6e91194da69f7a3
jagadeesh <jagadeeshmpl@gmail.com>
2016-06-11 23:12:09 +0800

    Jags Web applications

---
+++ api/bustimings.py
@@ -0,0 +1,75 @@
+__author__ = 'tester'
+import urllib3
+import urllib3.request
+import json
+from dateutil import parser
+import datetime
+import time
+from texttospeech import TextToSpeech
+
+class BusTimings:
+
+    __http = urllib3.PoolManager()
+    __proxy = urllib3.ProxyManager('http://webproxy.wlb2.nam.nsroot.net:8080')
+    __headers = {'AccountKey':'rvXT5s1ZSSKldpkizIhlag==','UniqueUserID':'984004a4-2685-4b30-8514-b3a75513c2bd', 'accept':'application/json'}
+
+    def getBusInformation(self,busStopId, serviceNo, busStopName='Your'):
+        print   'BusStopID=%s , ServiceNo=%s, busStopName=%s' % (busStopId, serviceNo, busStopName)
+        r = BusTimings.__http.request('GET', 'http://datamall2.mytransport.sg/ltaodataservice/BusArrival?BusStopID='+str(busStopId)+'&SST=True&ServiceNo='+str(serviceNo), headers=BusTimings.__headers)
+        #print r.status
+        #print r.data
+
+        jsonObj = json.loads(r.data)
+        r.close()
+        #print json.dumps(jsonObj, sort_keys=True, indent=4)
+
+        #print jsonObj['Services'][0]['NextBus']
+        #print jsonObj['Services'][0]['SubsequentBus']
+        #print jsonObj['Services'][0]['SubsequentBus3']
+
+        cdt = datetime.datetime.now()
+
+        firstBusArrivalDateTime = parser.parse(jsonObj['Services'][0]['NextBus']['EstimatedArrival'])
+        SecondBusArrivalDateTime = parser.parse(jsonObj['Services'][0]['SubsequentBus']['EstimatedArrival'])
+        thirdBusArrivalDateTime = parser.parse(jsonObj['Services'][0]['SubsequentBus3']['EstimatedArrival'])
+
+        firstBusLoad = jsonObj['Services'][0]['NextBus']['Load']
+        secondBusLoad = jsonObj['Services'][0]['SubsequentBus']['Load']
+
+        firstBusInMins = (int(time.mktime(firstBusArrivalDateTime.timetuple())) - int(time.time())) / 60
+        secondBusInMins = (int(time.mktime(SecondBusArrivalDateTime.timetuple())) - int(time.time())) / 60
+        thirdBusInMins = (int(time.mktime(thirdBusArrivalDateTime.timetuple())) - int(time.time())) / 60
+
+        message = '%s bus at %s bus stop, you have' % (serviceNo, busStopName)
+
+        if firstBusInMins <= 0:
+            message = '%s First Bus Arrived with %s.' % (message, firstBusLoad)
+        else:
+            message = '%s First Bus Arriving in %s minutes with %s.' % (message, firstBusInMins, firstBusLoad)
+
+        if secondBusInMins <= 0:
+            message = '%s Second Bus Arrived with %s. ' % (message, secondBusLoad)
+        else:
+            message = '%s Second Bus Arriving in %s minutes with %s.' % (message, secondBusInMins, secondBusLoad)
+
+        if thirdBusInMins >= 0:
+            message = '%s Third Bus Arriving in %s minutes.' % (message, thirdBusInMins)
+
+        print message
+        return message
+        #print 'you have First %s bus in %s mins it got %s, Second one in %s mins with %s and third one in %s mins' %
+        # (serviceNo, firstBusInMins, firstBusLoad, secondBusInMins,secondBusLoad,thirdBusInMins)
+
+
+if __name__=="__main__":
+
+    bus = BusTimings()
+    message = bus.getBusInformation(96129, 20, 'Melville Park')
+    tts = TextToSpeech()
+    audioFile = 'bus.wav'
+    tts.bluemixTTS(message, audioFile)
+    #tts.googleTTS("I am done for the day google", audioFile)
+    tts.play(audioFile)
+    
+    #getBusInformation(96371, 20, 'SCB CBP')
+    #getBusInformation(96319, 20, 'Yusen Logistics')
---
+++ api/texttospeech.py
@@ -0,0 +1,70 @@
+import requests
+import urllib
+import time
+import os
+ 
+class TextToSpeech:
+ 
+    __proxies = {"http": ""}
+    __bmttsHeaders = {'Authorization' : 'Basic YmZiMWY0MzUtZjc3MC00OThjLWJlNDYtNjVmZTE0ZTFjMjc2Ok9KWGVGbWhJeG4xWA=='}
+    __blumeixTTSURL =  "https://stream.watsonplatform.net/text-to-speech/api/v1/synthesize?accept=audio/wav&text=%s&voice=en-US_AllisonVoice"
+    __googleTTSURL = 'http://translate.google.com/translate_tts?ie=UTF-8&total=1&idx=0&textlen=32&client=tw-ob&q=%s&tl=EN-gb'
+    
+    def bluemixTTS(self, text, filePath):
+        try:
+            #print 'bluemixTTS : ' + text
+            finalText = urllib.quote(text)
+            #print finalText
+            finalurl = TextToSpeech.__blumeixTTSURL % finalText
+            #print finalurl
+ 
+            r = requests.get(finalurl,headers=TextToSpeech.__bmttsHeaders, verify=False)
+            #print r.status_code
+            #print r.headers
+            with open(filePath, 'wb') as f:
+                for chunk in r.iter_content(chunk_size=8192):
+                    if chunk: # filter out keep-alive new chunks
+                        f.write(chunk)
+                        #f.flush() commented by recommendation from J.F.Sebastian
+                print 'Done reading'
+                f.close()
+            r.close()
+        except ImportError:
+            print 'error'
+ 
+    def googleTTS(self,text, filePath):
+        try:
+
+            #print 'googleTTS : ' + text
+            finalText = urllib.quote(text)
+            #print finalText
+            finalurl = TextToSpeech.__googleTTSURL % finalText
+            #print finalurl
+ 
+            r = requests.get(finalurl,
+                         proxies=TextToSpeech.__proxies,
+                         verify=False)
+            #print r.status_code
+            #print r.headers
+            with open(filePath, 'wb') as f:
+                for chunk in r.iter_content(chunk_size=8192):
+                    if chunk: # filter out keep-alive new chunks
+                        f.write(chunk)
+                #f.flush() commented by recommendation from J.F.Sebastian
+                f.close()
+            r.close()
+ 
+ 
+        except ImportError:
+            print 'error'
+
+    def play(self, filePath):
+        os.system('omxplayer %s' % filePath)
+ 
+if __name__=="__main__":
+    tts = TextToSpeech()
+    audioFile = 'voice.wav'
+    tts.bluemixTTS("I am done for the day blue mix", audioFile)
+    #tts.googleTTS("I am done for the day google", audioFile)
+    tts.play(audioFile)
+    
